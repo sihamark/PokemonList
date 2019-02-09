2b46c0c92cc900d08a94deff9951b2dfda083158
Hans Markwart <hans@rapidrabbit.com>
2019-02-09 12:39:19 +0100

    - initial commit

---
+++ app/src/main/java/com/sihamark/pokemonlist/Importer.kt
@@ -0,0 +1,17 @@
+package com.sihamark.pokemonlist
+
+/**
+ * @author Hans Markwart (fanaloce@gmail.com)
+ *
+ * created at 07.02.2019.
+ */
+class Importer {
+
+    fun load() {
+
+    }
+
+    companion object {
+        private const val POKEMON_LIST = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+    }
+}
---
+++ app/src/main/java/com/sihamark/pokemonlist/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.sihamark.pokemonlist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.annotation.ContentView
+
+/**
+ * @author Hans Markwart (fanaloce@gmail.com)
+ *
+ * created at 07.02.2019.
+ */
+@ContentView(R.layout.activity_main)
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
